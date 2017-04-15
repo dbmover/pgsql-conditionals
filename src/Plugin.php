35a5f1995f2907a1c091f59bddd353506420f5c7
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-15 17:25:03 +0200

    expose database name

--- src/Plugin.php
+++ src/Plugin.php
@@ -16,11 +16,14 @@ class Plugin extends Conditionals\Plugin
 {
     protected function wrap(string $sql) : string
     {
+        $database = $this->loader->getDatabase();
         $tmp = 'tmp_'.md5(microtime(true));
         return <<<EOT
 DROP FUNCTION IF EXISTS $tmp();
 CREATE FUNCTION $tmp() RETURNS void AS $$
+DECLARE DBMOVER_DATABASE TEXT;
 BEGIN
+    DBMOVER_DATABASE := '$database';
     $sql
 END;
 $$ LANGUAGE 'plpgsql';
